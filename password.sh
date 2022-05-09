29b9dd7423dcd20c5351147c672d5dc934c9e4ea
16406 <you@example.com>
2022-05-09 17:27:34 +0800

    fix

--- password.sh
+++ password.sh
@@ -1,5 +1,8 @@
 username=$(cat /dev/urandom | head -n 16 | md5sum | head -c 8)
 password=$(cat /dev/urandom | head -n 16 | md5sum | head -c 8)
+admin_path=$(cat /dev/urandom | head -n 16 | md5sum | head -c 6)
 echo $username|bt 6
-echo $username|bt 5
-rm $0
+echo $password|bt 5
+echo $password > /www/server/panel/default.pl
+echo $admin_path > /www/server/panel/data/admin_path.pl
+rm -f $0
